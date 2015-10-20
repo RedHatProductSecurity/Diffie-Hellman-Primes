b47400ea0a0c77cecd48bbe3d1c91379417f0b57
Brad Knowles <brad@shub-internet.org>
2015-10-20 00:54:28 -0500

    Update generator.sh

--- brad@shub-internet.org/generator.sh
+++ brad@shub-internet.org/generator.sh
@@ -4,9 +4,9 @@ do
   mkdir -p ${BITS}
   for i in `seq 1 100`;
   do
-    openssl dhparam ${BITS} -text >> ${BITS}/$i
+    openssl dhparam ${BITS} -text >> ${BITS}/${i}
     git add .
-    git commit -am "${BITS} bits, ${i}/100"
+    git commit -am "${BITS}/${i}"
     git push
   done
 done
